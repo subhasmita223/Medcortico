046b4f6a253a9586acfcfd1411f2bad2d68e1a04
twinkle831 <dtwinkle831@gmail.com>
2024-08-30 17:12:58 +0530

    added landing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,36 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import Dashboard from './components/Dashboard';
-import Profile from './components/Profile'; 
+import Profile from './components/Profile';
+import LandingPage from './components/LandingPage';
+
+const Layout = ({ children }) => {
+  const location = useLocation();
+
+  // Check if the current route is "/landing"
+  const isLandingPage = location.pathname === '/';
+
+  return (
+    <div className={`flex ${isLandingPage ? 'w-full' : ''}`}>
+      {!isLandingPage && <Sidebar />} {/* Render Sidebar only if not on Landing Page */}
+      <main className={isLandingPage ? 'flex-1 w-full px-0' : 'flex-1 ml-64 md:ml-16'}>
+        {children}
+      </main>
+    </div>
+  );
+};
 
 const App = () => {
   return (
     <Router>
-      <div className="flex">
-        <Sidebar />
-        <main className="flex-1 ml-64 md:ml-16">
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/profile" element={<Profile />} />
-          </Routes>
-        </main>
-      </div>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </Layout>
     </Router>
   );
 };
---
+++ client/src/components/LandingPage.jsx
@@ -0,0 +1,25 @@
+// import Navbar from "../components/Navbar";
+import HeroSection from "./landing/HeroSection";
+import FeatureSection from "./landing/FeatureSection";
+import Workflow from "./landing/Workflow.jsx";
+import Footer from "./landing/Footer";
+import Pricing from "./landing/Pricing";
+import Testimonials from "./landing/Testimonials";
+
+const LandingPage = () => {
+  return (
+    <>
+  
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection />
+        <FeatureSection />
+        <Workflow />
+        <Pricing />
+        <Testimonials />
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default LandingPage;
